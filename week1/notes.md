7960a69d93bd296cbf299ebc916cf89f99ac5bf5
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-21 08:23:23 +0530

    Codealong week1 pset1 hello task

--- week1/notes.md
+++
@@ -1,7 +0,0 @@
-2022-10-19 20:28
-
-##### TOPICS
-
-## Correctness, Design, Style
-
-\*Copyright (c) 2022 Thaj Anaz. All Rights Reserved.
--- week1/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main( void ) {
-  // int    a = 1;
-  // int    b = 2;
-  // double c = 3;
-  printf( "Hello, world!" );
-}
