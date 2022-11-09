c023751d223b3bd9ce0c99498feb41333a3934d2
Thaj Anaz <anaz.thaj@gmail.com>
2022-11-10 01:57:56 +0530

    Finish v3.0 of Bubble sort - eliminate the entire loop if it's already sorted

--- week3/codealong/bubble-sort.c
+++ week3/codealong/bubble-sort.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 
-#define LENGTHOF( a ) ( &a )[ 1 ] - a
+#define LENGTHOF(a) (&a)[1] - a
 
-void bubble_sort( int a[], int length );
+void bubble_sort(int a[], int length);
 
-int main( void ) {
-  int    numbers[] = { 4,  6,  2,    78,   34,  52,    123, 123, 55,   65,   33,
-                       89, 90, 1234, 5542, 782, 23478, 232, 232, 1243, 5342, 78 };
-  size_t length    = LENGTHOF( numbers );
+int main(void) {
+  int    numbers[] = {4,  6,  2,    78,   34,  52,    123, 123, 55,   65,   33,
+                      89, 90, 1234, 5542, 782, 23478, 232, 232, 1243, 5342, 78};
+  size_t length    = LENGTHOF(numbers);
 
-  bubble_sort( numbers, length );
+  bubble_sort(numbers, length);
 
-  for ( int i = 0; i < length; i++ ) {
-    printf( "%d, ", numbers[ i ] );
+  for (int i = 0; i < length; i++) {
+    printf("%d, ", numbers[i]);
   }
 }
 
-void bubble_sort( int a[], int length ) {
-  for ( int i = 0; i < length; i++ ) {
+void bubble_sort(int a[], int length) {
+  int swapped = 0;
+  int i       = 0;
+
+  do {
+    swapped = 0;
     // length - 1 - i makes the program runs one less loop each time;
     // explanation at https://www.youtube.com/watch?v=YqzNgaFQEh8
-    for ( int j = 0; j < ( length - 1 - i ); j++ ) {
-      if ( a[ j ] > a[ j + 1 ] ) {
-        int temp   = a[ j ];
-        a[ j ]     = a[ j + 1 ];
-        a[ j + 1 ] = temp;
+    for (int j = 0; j < (length - 1 - i); j++) {
+      if (a[j] > a[j + 1]) {
+        int temp = a[j];
+        a[j]     = a[j + 1];
+        a[j + 1] = temp;
+        swapped  = 1;
       }
     }
-  }
+    i++;
+  } while (swapped == 0);
 }
