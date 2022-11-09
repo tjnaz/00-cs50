2505aae239a04e6333fb8d3bf280ec367fbac2c4
Thaj Anaz <anaz.thaj@gmail.com>
2022-11-09 10:29:50 +0530

    Complete numbers.c

---
+++ week3/codealong/numbers.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main( void ) {
+  int numbers[] = { 20, 500, 10, 5, 100, 1, 50 };
+  int userInput = get_int( "Enter a number to search: " );
+
+  for ( int i = 0; i < 7; i++ ) {
+    if ( numbers[ i ] == userInput ) {
+      printf( "Found\n" );
+      return 0;
+    }
+  }
+  printf( "Not found \n" );
+  return 1;
+}
