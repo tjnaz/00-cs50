b9d682a8a01f5907cba1bf8928e091936de62bc2
Thaj Anaz <anaz.thaj@gmail.com>
2022-11-09 22:33:17 +0530

    Finish v1.0 of Bubble sort

--- week3/codealong/bubble-sort.c
+++ week3/codealong/bubble-sort.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main( void ) {
-  int numbers[]   = { 4, 6, 2, 78, 34, 52, 123, 123, 55, 65 };
-  int lastElement = sizeof( numbers ) / sizeof( numbers[ 0 ] );
+  int numbers[] = { 4,  6,  2,    78,   34,  52,    123, 123, 55,   65,   33,
+                    89, 90, 1234, 5542, 782, 23478, 232, 232, 1243, 5342, 78 };
+  int length    = sizeof( numbers ) / sizeof( numbers[ 0 ] );
 
-  for ( int i = 0; i < sizeof( numbers ); i++ ) {
-    if ( numbers[ i ] > numbers[ i + 1 ] ) {
-      numbers[ i + 1 ] = numbers[ i ];
+  for ( int i = 0; i < length; i++ ) {
+    for ( int j = 0; j < ( length - 1 ); j++ ) {
+      if ( numbers[ j ] > numbers[ j + 1 ] ) {
+        int temp         = numbers[ j ];
+        numbers[ j ]     = numbers[ j + 1 ];
+        numbers[ j + 1 ] = temp;
+      }
     }
+    printf( "%d, ", numbers[ i ] );
   }
-  printf( "%i\n", numbers[ lastElement ] );
 }
